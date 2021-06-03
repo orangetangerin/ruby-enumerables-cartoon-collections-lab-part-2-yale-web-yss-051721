6cd6e61e26af9d9b127a67043e37ab76ab655c85
Jonathan Yu <jonathan.yu.jwy2@yale.edu>
2021-06-03 03:58:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,8 @@ def summon_captain_planet(planeteer_calls)
   # Return a new array of the results
   planeteer_calls.map do |string|
     string.capitalize
-    string.
+    concat(string, "!")
+  end
 end
 
 def long_planeteer_calls(planeteer_calls)
